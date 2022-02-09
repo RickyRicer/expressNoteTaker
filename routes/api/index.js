55eb28220193e18b1ecb98f017917d175b74b3fc
Rick Ansay <rick.ansay@gmail.com>
2022-02-08 22:07:47 -0700

    Fixing POST request, coming along nicely

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const noteRoutes = require('./notes');
 
+// This prepends /api to every route declared below this comment.
 router.use('/notes', noteRoutes);
 
 module.exports = router;
--- routes/api/notes/index.js
+++ routes/api/notes/index.js
@@ -1,15 +1,31 @@
 const router = require('express').Router();
-// const db = require('../../../db/db.json');
+const db = require('../../../db/db.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
 
 router.get('/', (req, res) => {
-    const db = fs.readFileSync('./db/db.json', 'utf8')
-    res.send(db);    
+    const content = fs.readFileSync('./db/db.json', 'utf8')
+    res.send(content);    
 });
 
 router.post('/', (req, res) => {
-    const db = fs.readFileSync('./db/db.json')
-    res.json(db);
+    const { title, text } = req.body;
+    const id = uuidv4();
+
+    if (!title || !text) {
+        res.send('Title and text cannot be blank!');
+    }
+
+    const newNote = {
+        id,
+        title,
+        text
+    };
+    const notes = JSON.stringify(newNote);
+    fs.writeFile(db, notes);
+    
+    res.json(JSON.stringify(newNote));
 });
 
 module.exports = router;
