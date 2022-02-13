e15c997c175fab2b24f71e1e45baa474e26ce3c3
Rick Ansay <rick.ansay@gmail.com>
2022-02-13 10:34:18 -0700

    POST and DELETE now working!

--- routes/api/notes/index.js
+++ routes/api/notes/index.js
@@ -1,31 +1,62 @@
 const router = require('express').Router();
-const db = require('../../../db/db.json');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 
 router.get('/', (req, res) => {
-    const content = fs.readFileSync('./db/db.json', 'utf8')
+    const content = fs.readFileSync('./db/db.json', 'utf8');
     res.send(content);    
 });
 
 router.post('/', (req, res) => {
+    const content = fs.readFileSync('./db/db.json', 'utf8');
+    
+    const db = JSON.parse(content);
+    
     const { title, text } = req.body;
     const id = uuidv4();
 
     if (!title || !text) {
         res.send('Title and text cannot be blank!');
-    }
+        return;
+    };
 
     const newNote = {
         id,
         title,
         text
     };
-    const notes = JSON.stringify(newNote);
-    fs.writeFile(db, notes);
+
+    // Get the current notes
+    console.log(db);    
+    // Add new notes to the file
+    db.push(newNote);
+    console.log(db);
+    // Write updated collection to db
+    const notes = JSON.stringify(db);
+    fs.writeFile('db/db.json', notes, function (err,res){
+        if (err) console.log('error', err);
+    });
     
-    res.json(JSON.stringify(newNote));
+    res.json(newNote);
+});
+
+router.delete('/:id', (req, res) => {
+    const content = fs.readFileSync('./db/db.json', 'utf8');
+    const db = JSON.parse(content);
+
+    res.json('ok');
+    const updatedNotes = [];
+    for (let i = 0; i < db.length; i++) {
+        const note = db[i];
+        if (note.id !== req.params.id){
+            updatedNotes.push(note);
+        };
+    };
+    const notes = JSON.stringify(updatedNotes);
+    fs.writeFile('db/db.json', notes, function (err, res){
+        if (err) console.log('error', err);
+    });
 });
 
 module.exports = router;
